7660d5f7ef0dfd3bcf446ce5745abefb0ca58532
vadiminshakov <vadiminshakov@gmail.com>
2020-08-03 01:20:51 +0300

    add Put method
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>

--- client/client.go
+++ client/client.go
@@ -12,12 +12,10 @@ type CommitClient struct {
 }
 
 func New(addr string) (*CommitClient, error) {
-
 	conn, err := grpc.Dial(addr, grpc.WithInsecure())
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to connect")
 	}
-
 	return &CommitClient{pb.NewCommitClient(conn)}, nil
 }
 
@@ -31,4 +29,8 @@ func (client *CommitClient) Precommit(*pb.PrecommitRequest) (*pb.Response, error
 
 func (client *CommitClient) Commit(*pb.CommitRequest) (*pb.Response, error) {
 	return nil, nil
+}
+
+func (client *CommitClient) Put(key string, value []byte) (*pb.Response, error) {
+	return client.Connection.Put(context.Background(), &pb.Entry{Key: key, Value: value})
 }
