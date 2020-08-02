a8c4698ac7c6b3c5b95909568f96c65d878cfae0
vadiminshakov <vadiminshakov@gmail.com>
2020-08-03 01:24:13 +0300

    simplify starter
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>

--- main.go
+++ main.go
@@ -1,29 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"github.com/vadiminshakov/committer/client"
 	"github.com/vadiminshakov/committer/config"
-	pb "github.com/vadiminshakov/committer/proto"
 	"github.com/vadiminshakov/committer/server"
 )
 
 func main() {
 	conf := config.Get()
-	s := server.NewCommitServer(conf.Nodeaddr)
-	if conf.Role == "coordinator" {
-		for _, node := range conf.Followers {
-			cli, err := client.New(node)
-			if err != nil {
-				panic("failed to create client")
-			}
-			response, err := cli.Propose(&pb.ProposeRequest{})
-			if err != nil {
-				panic(err)
-			}
-			fmt.Println(response.Type)
-		}
-	} else {
-		server.Run(s)
+	s, err := server.NewCommitServer(conf.Nodeaddr, server.WithFollowers(conf.Followers))
+	if err != nil {
+		panic(err)
 	}
+	s.Run()
 }
