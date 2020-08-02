e9a12a20055897f8e2b6cf5d0613b34c90c7a2ad
vadiminshakov <vadiminshakov@gmail.com>
2020-08-02 17:16:29 +0300

    add configurator
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>

---
+++ config/config.go
@@ -0,0 +1,73 @@
+package config
+
+import (
+	"flag"
+	"github.com/spf13/viper"
+	"strings"
+)
+
+type Config struct {
+	Role        string
+	Nodeaddr    string
+	Coordinator string
+	Followers   []string
+}
+
+type followers []string
+
+func (i *followers) String() string {
+	return strings.Join(*i, ",")
+}
+
+func (i *followers) Set(value string) error {
+	*i = append(*i, value)
+	return nil
+}
+
+func Get() *Config {
+	// command-line flags
+	var followersArray followers
+	config := flag.String("config", "", "path to config")
+	role := flag.String("role", "follower", "role (coordinator of follower)")
+	nodeaddr := flag.String("nodeaddr", "localhost:3050", "node address")
+	coordinator := flag.String("coordinator", "", "coordinator address")
+	flag.Var(&followersArray, "follower", "follower address")
+	flag.Parse()
+
+	// viper configuration
+	var configFromFile Config
+	viper.SetDefault("./", "content")
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	err := viper.Unmarshal(&configFromFile)
+	if err != nil {
+		panic("Unable to unmarshal config")
+	}
+
+	if *config == "" {
+		if *role != "coordinator" {
+			if !includes(followersArray, *nodeaddr) {
+				followersArray = append(followersArray, *nodeaddr)
+			}
+		}
+		return &Config{*role, *nodeaddr, *coordinator, followersArray}
+	}
+
+	if *role != "coordinator" {
+		if !includes(configFromFile.Followers, configFromFile.Nodeaddr) {
+			configFromFile.Followers = append(configFromFile.Followers, configFromFile.Nodeaddr)
+		}
+	}
+
+	return &Config{configFromFile.Role, configFromFile.Nodeaddr, configFromFile.Coordinator, configFromFile.Followers}
+}
+
+func includes(arr []string, value string) bool {
+	for i := range arr {
+		if arr[i] == value {
+			return true
+		}
+	}
+	return false
+}
