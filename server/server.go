a9221b86fb5688ae06b34d83e755fd51ba00a88b
vadiminshakov <vadiminshakov@gmail.com>
2020-08-03 01:23:04 +0300

    add Put method and functional options support
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>

--- server/server.go
+++ server/server.go
@@ -2,6 +2,8 @@ package server
 
 import (
 	"context"
+	"github.com/vadiminshakov/committer/client"
+	"github.com/vadiminshakov/committer/config"
 	"github.com/vadiminshakov/committer/core"
 	pb "github.com/vadiminshakov/committer/proto"
 	"google.golang.org/grpc"
@@ -11,8 +13,12 @@ import (
 	"net"
 )
 
+type Option func(server *Server)
+
 type Server struct {
-	Addr string
+	Addr      string
+	Followers []*client.CommitClient
+	Config    config.Config
 }
 
 func (s *Server) Propose(ctx context.Context, req *pb.ProposeRequest) (*pb.Response, error) {
@@ -25,18 +31,54 @@ func (s *Server) Commit(ctx context.Context, req *pb.CommitRequest) (*pb.Respons
 	return nil, status.Errorf(codes.Unimplemented, "method Commit not implemented")
 }
 
-func NewCommitServer(addr string) *Server {
-	return &Server{Addr: addr}
+func (s *Server) Put(ctx context.Context, req *pb.Entry) (*pb.Response, error) {
+	var (
+		response *pb.Response
+		err      error
+	)
+	for _, follower := range s.Followers {
+		response, err = follower.Propose(&pb.ProposeRequest{Key: req.Key,
+			Value:      req.Value,
+			CommitType: pb.CommitType_TWO_PHASE_COMMIT,
+			Index:      1})
+		if err != nil {
+			return nil, status.Error(codes.Internal, err.Error())
+		}
+		if response.Type != pb.Type_ACK {
+			return nil, status.Error(codes.Internal, "follower not acknowledged msg")
+		}
+	}
+	return &pb.Response{Type: pb.Type_ACK}, nil
 }
 
-func Run(serv *Server) {
-	grpcServer := grpc.NewServer()
-	pb.RegisterCommitServer(grpcServer, serv)
+func NewCommitServer(addr string, opts ...Option) (*Server, error) {
+	server := &Server{Addr: addr}
+	for _, option := range opts {
+		option(server)
+	}
+	return server, nil
+}
 
-	l, err := net.Listen("tcp", serv.Addr)
+func WithFollowers(followers []string) func(*Server) {
+	return func(server *Server) {
+		for _, node := range followers {
+			cli, err := client.New(node)
+			if err != nil {
+				panic(err)
+			}
+			server.Followers = append(server.Followers, cli)
+		}
+	}
+}
+
+func (s *Server) Run() {
+	grpcServer := grpc.NewServer()
+	pb.RegisterCommitServer(grpcServer, s)
+
+	l, err := net.Listen("tcp", s.Addr)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	log.Printf("Listening on tcp://%s", serv.Addr)
+	log.Printf("Listening on tcp://%s", s.Addr)
 	grpcServer.Serve(l)
 }
