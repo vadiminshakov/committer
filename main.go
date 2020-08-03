579bc04cbf0746bcd1575f4fcb24b6b9eb3e3bfc
vadiminshakov <vadiminshakov@gmail.com>
2020-08-03 20:41:18 +0300

    add graceful shutdown
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>

--- main.go
+++ main.go
@@ -3,13 +3,21 @@ package main
 import (
 	"github.com/vadiminshakov/committer/config"
 	"github.com/vadiminshakov/committer/server"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
 	conf := config.Get()
-	s, err := server.NewCommitServer(conf.Nodeaddr, server.WithFollowers(conf.Followers))
+	s, err := server.NewCommitServer(conf.Nodeaddr, server.WithFollowers(conf.Followers), server.WithConfig(conf))
 	if err != nil {
 		panic(err)
 	}
 	s.Run()
+	<-ch
+	s.Stop()
 }
