c34f5de327cc9a9cb03eb25d69f92eea92db6691
vadiminshakov <vadiminshakov@gmail.com>
2020-08-04 23:37:11 +0300

    add badgerdb impl
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>

---
+++ db/badger.go
@@ -0,0 +1,44 @@
+package db
+
+import (
+	"fmt"
+	badger "github.com/dgraph-io/badger/v2"
+)
+
+type BadgerDB struct {
+	db *badger.DB
+}
+
+func New(path string) (Database, error) {
+	db, err := badger.Open(badger.DefaultOptions(path))
+	if err != nil {
+		return nil, err
+	}
+	return &BadgerDB{db}, nil
+}
+
+func (b *BadgerDB) Put(key string, value []byte) error {
+	return b.db.Update(func(txn *badger.Txn) error {
+		return txn.Set([]byte(key), value)
+	})
+}
+
+func (b *BadgerDB) Get(key string) ([]byte, error) {
+	var value []byte
+	err := b.db.View(func(txn *badger.Txn) error {
+		item, err := txn.Get([]byte(key))
+		if err != nil {
+			return err
+		}
+		err = item.Value(func(val []byte) error {
+			value = append([]byte{}, val...)
+			return nil
+		})
+		return err
+	})
+	return value, err
+}
+
+func (b *BadgerDB) Close() error {
+	return b.db.Close()
+}
