74e2be18716673092282f5796b456e34e20e7d13
vadiminshakov <vadiminshakov@gmail.com>
2020-08-06 13:27:06 +0300

    improve peer client
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>

--- peer/peer.go
+++ peer/peer.go
@@ -20,20 +20,20 @@ func New(addr string) (*CommitClient, error) {
 	return &CommitClient{pb.NewCommitClient(conn)}, nil
 }
 
-func (client *CommitClient) Propose(*pb.ProposeRequest) (*pb.Response, error) {
-	return client.Connection.Propose(context.Background(), &pb.ProposeRequest{Key: "hi", Value: []byte("hello"), CommitType: pb.CommitType_TWO_PHASE_COMMIT, Index: 1})
+func (client *CommitClient) Propose(ctx context.Context, req *pb.ProposeRequest) (*pb.Response, error) {
+	return client.Connection.Propose(ctx, req)
 }
 
-func (client *CommitClient) Precommit(*pb.PrecommitRequest) (*pb.Response, error) {
-	return nil, nil
+func (client *CommitClient) Precommit(ctx context.Context, req *pb.PrecommitRequest) (*pb.Response, error) {
+	return client.Connection.Precommit(ctx, req)
 }
 
-func (client *CommitClient) Commit(*pb.CommitRequest) (*pb.Response, error) {
-	return nil, nil
+func (client *CommitClient) Commit(ctx context.Context, req *pb.CommitRequest) (*pb.Response, error) {
+	return client.Connection.Commit(ctx, req)
 }
 
 // Put sends key/value pair to peer (it should be a coordinator).
 // The coordinator reaches consensus and all peers commit the value.
-func (client *CommitClient) Put(key string, value []byte) (*pb.Response, error) {
-	return client.Connection.Put(context.Background(), &pb.Entry{Key: key, Value: value})
+func (client *CommitClient) Put(ctx context.Context, key string, value []byte) (*pb.Response, error) {
+	return client.Connection.Put(ctx, &pb.Entry{Key: key, Value: value})
 }
