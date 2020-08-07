ebbb8091fc5afe7e51e947e92913d7f1e457d460
vadiminshakov <vadiminshakov@gmail.com>
2020-08-07 12:48:15 +0300

    add comments and deps
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>

--- peer/peer.go
+++ peer/peer.go
@@ -1,9 +1,9 @@
 package peer
 
 import (
+	"context"
 	"github.com/pkg/errors"
 	pb "github.com/vadiminshakov/committer/proto"
-	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 )
 
@@ -12,12 +12,13 @@ type CommitClient struct {
 }
 
 // New creates instance of peer client.
+// 'addr' is a coordinator network address (host + port).
 func New(addr string) (*CommitClient, error) {
 	conn, err := grpc.Dial(addr, grpc.WithInsecure())
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to connect")
 	}
-	return &CommitClient{pb.NewCommitClient(conn)}, nil
+	return &CommitClient{Connection: pb.NewCommitClient(conn)}, nil
 }
 
 func (client *CommitClient) Propose(ctx context.Context, req *pb.ProposeRequest) (*pb.Response, error) {
