91e18034bd39af28eb1cc5952c1165004e0f6346
vadiminshakov <vadiminshakov@gmail.com>
2020-08-09 23:59:50 +0300

    add hooks
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>

---
+++ hooks/hooks.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	pb "github.com/vadiminshakov/committer/proto"
+)
+
+func Propose(req *pb.ProposeRequest) bool {
+	return true
+}
+
+func Commit(req *pb.CommitRequest) bool {
+	return true
+}
---
+++ hooks/hooksfabric.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"errors"
+	pb "github.com/vadiminshakov/committer/proto"
+	"github.com/vadiminshakov/committer/server"
+	"plugin"
+)
+
+type ProposeHook func(req *pb.ProposeRequest) bool
+type CommitHook func(req *pb.CommitRequest) bool
+
+func Hooks(hooksPath string) ([]server.Option, error) {
+	plug, err := plugin.Open(hooksPath)
+	if err != nil {
+		return nil, err
+	}
+
+	WithProposeHook, err := plug.Lookup("Propose")
+	if err != nil {
+		return nil, err
+	}
+	proposeFunc, ok := WithProposeHook.(ProposeHook)
+	if !ok {
+		return nil, errors.New("failed to assert WithProposeHook to server.Option")
+	}
+	WithCommitHook, err := plug.Lookup("Commit")
+	if err != nil {
+		return nil, err
+	}
+	commitFunc, ok := WithCommitHook.(CommitHook)
+	if !ok {
+		return nil, errors.New("failed to assert WithCommitHook to server.Option")
+	}
+
+	proposeHook := func(f ProposeHook) func(*server.Server) error {
+		return func(server *server.Server) error {
+			server.ProposeHook = f
+			return nil
+		}
+	}
+	commitHook := func(f CommitHook) func(*server.Server) error {
+		return func(server *server.Server) error {
+			server.CommitHook = f
+			return nil
+		}
+	}
+
+	return []server.Option{proposeHook(proposeFunc), commitHook(commitFunc)}, nil
+}
