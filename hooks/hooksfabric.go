8469df52f66561b095215d6a33d9aae05b53b827
vadiminshakov <vadiminshakov@gmail.com>
2020-08-10 18:48:36 +0300

    del plugins, use .go files

--- hooks/hooksfabric.go
+++ hooks/hooksfabric.go
@@ -1,38 +1,15 @@
 package hooks
 
 import (
-	"errors"
+	"github.com/vadiminshakov/committer/hooks/src"
 	pb "github.com/vadiminshakov/committer/proto"
 	"github.com/vadiminshakov/committer/server"
-	"plugin"
 )
 
 type ProposeHook func(req *pb.ProposeRequest) bool
 type CommitHook func(req *pb.CommitRequest) bool
 
 func Get(hooksPath string) ([]server.Option, error) {
-	plug, err := plugin.Open(hooksPath)
-	if err != nil {
-		return nil, err
-	}
-
-	WithProposeHook, err := plug.Lookup("Propose")
-	if err != nil {
-		return nil, err
-	}
-	proposeFunc, ok := WithProposeHook.(func(req *pb.ProposeRequest) bool)
-	if !ok {
-		return nil, errors.New("failed to assert WithProposeHook to server.Option")
-	}
-	WithCommitHook, err := plug.Lookup("Commit")
-	if err != nil {
-		return nil, err
-	}
-	commitFunc, ok := WithCommitHook.(func(req *pb.CommitRequest) bool)
-	if !ok {
-		return nil, errors.New("failed to assert WithCommitHook to server.Option")
-	}
-
 	proposeHook := func(f ProposeHook) func(*server.Server) error {
 		return func(server *server.Server) error {
 			server.ProposeHook = f
@@ -45,6 +22,5 @@ func Get(hooksPath string) ([]server.Option, error) {
 			return nil
 		}
 	}
-
-	return []server.Option{proposeHook(proposeFunc), commitHook(commitFunc)}, nil
+	return []server.Option{proposeHook(src.Propose), commitHook(src.Commit)}, nil
 }
