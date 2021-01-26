a8bb3f3c1f5b9dfaaa8e3c54fa6f747aaade82e6
vadiminshakov <vadiminshakov@gmail.com>
2021-01-26 20:07:04 +0500

    convert funcs to methods, add index to Response msg
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>

--- server/committer.go
+++ server/committer.go
@@ -4,35 +4,37 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"github.com/vadiminshakov/committer/cache"
 	"github.com/vadiminshakov/committer/db"
 	pb "github.com/vadiminshakov/committer/proto"
 	"log"
 )
 
-func ProposeHandler(ctx context.Context, req *pb.ProposeRequest, hook func(req *pb.ProposeRequest) bool, nodecache *cache.Cache) (*pb.Response, error) {
+func (s *Server) ProposeHandler(ctx context.Context, req *pb.ProposeRequest, hook func(req *pb.ProposeRequest) bool) (*pb.Response, error) {
 	var response *pb.Response
 	if hook(req) {
 		log.Printf("Received: %s=%s\n", req.Key, string(req.Value))
-		nodecache.Set(req.Index, req.Key, req.Value)
-		response = &pb.Response{Type: pb.Type_ACK}
+		s.NodeCache.Set(req.Index, req.Key, req.Value)
+		response = &pb.Response{Type: pb.Type_ACK, Index: req.Index}
 	} else {
-		response = &pb.Response{Type: pb.Type_NACK}
+		response = &pb.Response{Type: pb.Type_NACK, Index: req.Index}
+	}
+	if s.Height > req.Index {
+		response = &pb.Response{Type: pb.Type_NACK, Index: s.Height}
 	}
 	return response, nil
 }
 
-func PrecommitHandler(ctx context.Context, req *pb.PrecommitRequest) (*pb.Response, error) {
+func (s *Server) PrecommitHandler(ctx context.Context, req *pb.PrecommitRequest) (*pb.Response, error) {
 	return &pb.Response{Type: pb.Type_ACK}, nil
 }
 
-func CommitHandler(ctx context.Context, req *pb.CommitRequest, hook func(req *pb.CommitRequest) bool, db db.Database, nodecache *cache.Cache) (*pb.Response, error) {
+func (s *Server) CommitHandler(ctx context.Context, req *pb.CommitRequest, hook func(req *pb.CommitRequest) bool, db db.Database) (*pb.Response, error) {
 	var response *pb.Response
 	if hook(req) {
 		log.Printf("Committing on height: %d\n", req.Index)
-		key, value, ok := nodecache.Get(req.Index)
+		key, value, ok := s.NodeCache.Get(req.Index)
 		if !ok {
-			nodecache.Delete(req.Index)
+			s.NodeCache.Delete(req.Index)
 			return &pb.Response{Type: pb.Type_NACK}, errors.New(fmt.Sprintf("no value in node cache on the index %d", req.Index))
 		}
 
@@ -41,7 +43,7 @@ func CommitHandler(ctx context.Context, req *pb.CommitRequest, hook func(req *pb
 		}
 		response = &pb.Response{Type: pb.Type_ACK}
 	} else {
-		nodecache.Delete(req.Index)
+		s.NodeCache.Delete(req.Index)
 		response = &pb.Response{Type: pb.Type_NACK}
 	}
 	return response, nil
