72ca44becfc21bb6ef8ecc76eb2da9e72c67c169
vadiminshakov <vadiminshakov@gmail.com>
2021-01-26 20:05:06 +0500

    add grpc connection params
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>

--- peer/peer.go
+++ peer/peer.go
@@ -8,6 +8,8 @@ import (
 	"github.com/pkg/errors"
 	pb "github.com/vadiminshakov/committer/proto"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/backoff"
+	"time"
 )
 
 type CommitClient struct {
@@ -22,10 +24,18 @@ func New(addr string, tracer *zipkin.Tracer) (*CommitClient, error) {
 		conn *grpc.ClientConn
 		err  error
 	)
+
+	connParams := grpc.ConnectParams{
+		Backoff: backoff.Config{
+			BaseDelay: 100 * time.Millisecond,
+			MaxDelay:  5 * time.Second,
+		},
+		MinConnectTimeout: 200 * time.Millisecond,
+	}
 	if tracer != nil {
-		conn, err = grpc.Dial(addr, grpc.WithInsecure(), grpc.WithStatsHandler(zipkingrpc.NewClientHandler(tracer)))
+		conn, err = grpc.Dial(addr, grpc.WithConnectParams(connParams), grpc.WithInsecure(), grpc.WithStatsHandler(zipkingrpc.NewClientHandler(tracer)))
 	} else {
-		conn, err = grpc.Dial(addr, grpc.WithInsecure())
+		conn, err = grpc.Dial(addr, grpc.WithConnectParams(connParams), grpc.WithInsecure())
 	}
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to connect")
