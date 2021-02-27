551896a275fbe27924cf18d9b6d3342afa696e17
vadiminshakov <vadiminshakov@gmail.com>
2021-02-27 13:02:50 +0500

    add logs to hooks
    Signed-off-by: vadiminshakov <vadiminshakov@gmail.com>

--- hooks/src/hooks.go
+++ hooks/src/hooks.go
@@ -1,13 +1,16 @@
 package src
 
 import (
+	log "github.com/sirupsen/logrus"
 	pb "github.com/vadiminshakov/committer/proto"
 )
 
 func Propose(req *pb.ProposeRequest) bool {
+	log.Infof("propose hook on height %d is OK", req.Index)
 	return true
 }
 
 func Commit(req *pb.CommitRequest) bool {
+	log.Infof("commit hook on height %d is OK", req.Index)
 	return true
 }
