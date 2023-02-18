4c9c8180e3ab49bb051a9b7ae43d8e058a61edad
Vadim Inshakov <vadiminshakov@gmail.com>
2023-02-19 04:38:25 +0500

    use Committer iface in handlers

--- server/committer.go
+++ server/committer.go
@@ -2,49 +2,51 @@ package server
 
 import (
 	"context"
-	"errors"
-	"fmt"
-	log "github.com/sirupsen/logrus"
-	"github.com/vadiminshakov/committer/db"
+	"github.com/vadiminshakov/committer/entity"
 	pb "github.com/vadiminshakov/committer/proto"
 )
 
-func (s *Server) ProposeHandler(ctx context.Context, req *pb.ProposeRequest, hook func(req *pb.ProposeRequest) bool) (*pb.Response, error) {
-	var response *pb.Response
-	if hook(req) {
-		log.Infof("received: %s=%s\n", req.Key, string(req.Value))
-		s.NodeCache.Set(req.Index, req.Key, req.Value)
-		response = &pb.Response{Type: pb.Type_ACK, Index: req.Index}
-	} else {
-		response = &pb.Response{Type: pb.Type_NACK, Index: req.Index}
+func (s *Server) ProposeHandler(ctx context.Context, req *pb.ProposeRequest) (*pb.Response, error) {
+	r := &entity.ProposeRequest{
+		Key:    req.Key,
+		Value:  req.Value,
+		Height: req.Index,
 	}
-	if s.Height > req.Index {
-		response = &pb.Response{Type: pb.Type_NACK, Index: s.Height}
+	resp, err := s.committer.Propose(ctx, r)
+	if err != nil {
+		return nil, err
 	}
-	return response, nil
+	respPb := &pb.Response{
+		Type:  pb.Type(resp.ResponseType),
+		Index: resp.Height,
+	}
+	return respPb, nil
 }
 
 func (s *Server) PrecommitHandler(ctx context.Context, req *pb.PrecommitRequest) (*pb.Response, error) {
-	return &pb.Response{Type: pb.Type_ACK}, nil
+	resp, err := s.committer.Precommit(ctx, req.Index)
+	if err != nil {
+		return nil, err
+	}
+	respPb := &pb.Response{
+		Type:  pb.Type(resp.ResponseType),
+		Index: resp.Height,
+	}
+	return respPb, nil
 }
 
-func (s *Server) CommitHandler(ctx context.Context, req *pb.CommitRequest, hook func(req *pb.CommitRequest) bool, db db.Database) (*pb.Response, error) {
-	var response *pb.Response
-	if hook(req) {
-		log.Printf("Committing on height: %d\n", req.Index)
-		key, value, ok := s.NodeCache.Get(req.Index)
-		if !ok {
-			s.NodeCache.Delete(req.Index)
-			return &pb.Response{Type: pb.Type_NACK}, errors.New(fmt.Sprintf("no value in node cache on the index %d", req.Index))
-		}
-
-		if err := db.Put(key, value); err != nil {
-			return nil, err
-		}
-		response = &pb.Response{Type: pb.Type_ACK}
-	} else {
-		s.NodeCache.Delete(req.Index)
-		response = &pb.Response{Type: pb.Type_NACK}
+func (s *Server) CommitHandler(ctx context.Context, req *pb.CommitRequest) (*pb.Response, error) {
+	r := &entity.CommitRequest{
+		Height:     req.Index,
+		IsRollback: req.IsRollback,
 	}
-	return response, nil
+	resp, err := s.committer.Commit(ctx, r)
+	if err != nil {
+		return nil, err
+	}
+	respPb := &pb.Response{
+		Type:  pb.Type(resp.ResponseType),
+		Index: resp.Height,
+	}
+	return respPb, nil
 }
