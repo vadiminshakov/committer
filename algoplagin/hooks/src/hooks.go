5269aaaa6b5df0b7d61b5292290830cd092d4f9e
Vadim Inshakov <vadiminshakov@gmail.com>
2023-02-19 04:39:22 +0500

    mv hooks

---
+++ algoplagin/hooks/src/hooks.go
@@ -0,0 +1,16 @@
+package src
+
+import (
+	log "github.com/sirupsen/logrus"
+	"github.com/vadiminshakov/committer/entity"
+)
+
+func Propose(req *entity.ProposeRequest) bool {
+	log.Infof("propose hook on height %d is OK", req.Height)
+	return true
+}
+
+func Commit(req *entity.CommitRequest) bool {
+	log.Infof("commit hook on height %d is OK", req.Height)
+	return true
+}
