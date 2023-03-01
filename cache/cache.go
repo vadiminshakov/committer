8a8153e2b57c0c099a2e2fd20dd250cc3cc4dd13
Vadim Inshakov <vadiminshakov@gmail.com>
2023-03-02 02:05:59 +0500

    add votes methods to cache

--- cache/cache.go
+++ cache/cache.go
@@ -1,6 +1,7 @@
 package cache
 
 import (
+	"github.com/vadiminshakov/committer/entity"
 	"sync"
 )
 
@@ -10,30 +11,50 @@ type msg struct {
 }
 
 type Cache struct {
-	store map[uint64]msg
-	mu    sync.RWMutex
+	kv      map[uint64]msg
+	votes   map[uint64][]*entity.Vote
+	muKv    sync.RWMutex
+	muVotes sync.RWMutex
 }
 
 func New() *Cache {
-	hashtable := make(map[uint64]msg)
-	return &Cache{store: hashtable}
+	kvstore, votesstore := make(map[uint64]msg), make(map[uint64][]*entity.Vote)
+	return &Cache{kv: kvstore, votes: votesstore}
 }
 
 func (c *Cache) Set(index uint64, key string, value []byte) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	c.store[index] = msg{key, value}
+	c.muKv.Lock()
+	defer c.muKv.Unlock()
+	c.kv[index] = msg{key, value}
 }
 
 func (c *Cache) Get(index uint64) (string, []byte, bool) {
-	c.mu.RLock()
-	defer c.mu.RUnlock()
-	message, ok := c.store[index]
+	c.muKv.RLock()
+	defer c.muKv.RUnlock()
+	message, ok := c.kv[index]
 	return message.Key, message.Value, ok
 }
 
 func (c *Cache) Delete(index uint64) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	delete(c.store, index)
+	c.muKv.Lock()
+	delete(c.kv, index)
+	c.muKv.Unlock()
+}
+
+func (c *Cache) SetVotes(index uint64, votes []*entity.Vote) {
+	c.muVotes.Lock()
+	defer c.muVotes.Unlock()
+	c.votes[index] = append(c.votes[index], votes...)
+}
+
+func (c *Cache) GetVotes(index uint64) []*entity.Vote {
+	c.muVotes.RLock()
+	defer c.muVotes.RUnlock()
+	return c.votes[index]
+}
+
+func (c *Cache) DelVotes(index uint64) {
+	c.muVotes.Lock()
+	delete(c.votes, index)
+	c.muVotes.Unlock()
 }
