928e51cae2ca039b4e392671043515bf3b1cffbf
Vadim Inshakov <vadiminshakov@gmail.com>
2023-03-12 02:26:27 +0500

    refactor

---
+++ cohort/entity.go
@@ -0,0 +1,12 @@
+package cohort
+
+import "github.com/vadiminshakov/committer/entity"
+
+type PrecommitRequest struct {
+	Index uint64         `protobuf:"varint,1,opt,name=index,proto3" json:"index,omitempty"`
+	Votes []*entity.Vote `protobuf:"bytes,2,rep,name=votes,proto3" json:"votes,omitempty"`
+}
+
+type mode string
+
+const THREE_PHASE mode = "three-phase"
