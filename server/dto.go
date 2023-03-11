797e0d976f23a23c1351af4e4f969e053cf06e50
Vadim Inshakov <vadiminshakov@gmail.com>
2023-03-12 02:26:21 +0500

    refactor

---
+++ server/dto.go
@@ -0,0 +1,55 @@
+package server
+
+import (
+	"github.com/vadiminshakov/committer/entity"
+	"github.com/vadiminshakov/committer/proto"
+)
+
+func proposeRequestPbToEntity(request *proto.ProposeRequest) *entity.ProposeRequest {
+	if request == nil {
+		return nil
+	}
+
+	return &entity.ProposeRequest{
+		Key:    request.Key,
+		Value:  request.Value,
+		Height: request.Index,
+	}
+}
+
+func commitRequestPbToEntity(request *proto.CommitRequest) *entity.CommitRequest {
+	if request == nil {
+		return nil
+	}
+
+	return &entity.CommitRequest{
+		Height:     request.Index,
+		IsRollback: request.IsRollback,
+	}
+}
+
+func entityResponseToPb(e *entity.Response) *proto.Response {
+	if e == nil {
+		return nil
+	}
+	return &proto.Response{
+		Type:  proto.Type(e.ResponseType),
+		Index: e.Height,
+	}
+}
+
+func votesPbToEntity(votes []*proto.Vote) []*entity.Vote {
+	if votes == nil {
+		return nil
+	}
+
+	newVotes := make([]*entity.Vote, 0, len(votes))
+	for _, v := range votes {
+		newVotes = append(newVotes, &entity.Vote{
+			Node:       v.Node,
+			IsAccepted: v.IsAccepted,
+		})
+	}
+
+	return newVotes
+}
