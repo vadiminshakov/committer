5a186bf697241b00ca7c0c12ff02c9cd7dc1f15e
Vadim Inshakov <vadiminshakov@gmail.com>
2023-03-13 23:53:22 +0500

    refactor

--- io/db/badger.go
+++ io/db/badger.go
@@ -8,7 +8,7 @@ type BadgerDB struct {
 	db *badger.DB
 }
 
-func New(path string) (Database, error) {
+func New(path string) (Repository, error) {
 	db, err := badger.Open(badger.DefaultOptions(path).WithLogger(nil))
 	if err != nil {
 		return nil, err
