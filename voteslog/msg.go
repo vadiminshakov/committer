34c8a14a98ff8616510dc89e3adc50b69397a646
Vadim Inshakov <vadiminshakov@gmail.com>
2023-09-11 01:47:47 +0500

    refactor

--- voteslog/msg.go
+++ voteslog/msg.go
@@ -3,12 +3,20 @@ package voteslog
 import "github.com/vadiminshakov/committer/core/entity"
 
 type msg struct {
-	Index uint64
+	Idx   uint64
 	Key   string
 	Value []byte
 }
 
+func (m msg) Index() uint64 {
+	return m.Idx
+}
+
 type votesMsg struct {
-	Index uint64
+	Idx   uint64
 	Votes []*entity.Vote
+}
+
+func (v votesMsg) Index() uint64 {
+	return v.Idx
 }
