4e1d920e23d343a604026d5f515f95c1cb9f78ad
Vadim Inshakov <vadiminshakov@gmail.com>
2023-10-01 21:31:34 +0500

    change client example

--- examples/client/client.go
+++ examples/client/client.go
@@ -6,12 +6,13 @@ import (
 	"github.com/vadiminshakov/committer/io/gateway/grpc/client"
 	pb "github.com/vadiminshakov/committer/io/gateway/grpc/proto"
 	"github.com/vadiminshakov/committer/io/trace"
+	"strconv"
 )
 
 const coordinatorAddr = "localhost:3000"
 
 func main() {
-	key, value := "somekey", []byte("somevalue")
+	key, value := "somekey", "somevalue"
 	tracer, err := trace.Tracer("client", coordinatorAddr)
 	if err != nil {
 		panic(err)
@@ -23,19 +24,21 @@ func main() {
 		panic(err)
 	}
 
-	// put a key-value pair
-	resp, err := cli.Put(context.Background(), key, value)
-	if err != nil {
-		panic(err)
-	}
-	if resp.Type != pb.Type_ACK {
-		panic("msg is not acknowledged")
-	}
+	for i := 0; i < 5; i++ {
+		// put a key-value pair
+		resp, err := cli.Put(context.Background(), key+strconv.Itoa(i), []byte(value+strconv.Itoa(i)))
+		if err != nil {
+			panic(err)
+		}
+		if resp.Type != pb.Type_ACK {
+			panic("msg is not acknowledged")
+		}
 
-	// read committed key
-	v, err := cli.Get(context.Background(), key)
-	if err != nil {
-		panic(err)
+		// read committed key
+		v, err := cli.Get(context.Background(), key+strconv.Itoa(i))
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("got value for key '%s': %s", key+strconv.Itoa(i), string(v.Value)+strconv.Itoa(i))
 	}
-	fmt.Printf("got value for key '%s': %s", key, v.Value)
 }
