bd6b7b2638949dbc8803a015398de4cd445b3635
Vadim Inshakov <vadiminshakov@gmail.com>
2024-12-27 14:33:09 +0500

    del unused

--- core/cohort/entity.go
+++ core/cohort/entity.go
@@ -1,12 +1,5 @@
 package cohort
 
-import "github.com/vadiminshakov/committer/core/dto"
-
-type PrecommitRequest struct {
-	Votes []*dto.Vote `protobuf:"bytes,2,rep,name=votes,proto3" json:"votes,omitempty"`
-	Index uint64      `protobuf:"varint,1,opt,name=index,proto3" json:"index,omitempty"`
-}
-
 type Mode string
 
 const THREE_PHASE Mode = "three-phase"
