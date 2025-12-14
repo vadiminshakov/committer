c6107a4db4f4f2e4ac4e6f7e41b460a2b198dde4
Vadim Inshakov <vadiminshakov@gmail.com>
2025-12-14 13:53:30 +0500

    feat: change wal msg encoding

--- core/walrecord/walrecord.go
+++ core/walrecord/walrecord.go
@@ -1,7 +1,7 @@
-package walproto
+package walrecord
 
 import (
-	"encoding/json"
+	"encoding/binary"
 	"fmt"
 )
 
@@ -18,8 +18,8 @@ const (
 
 // WalTx represents the payload stored in the WAL.
 type WalTx struct {
-	Key   string `json:"k"`
-	Value []byte `json:"v"`
+	Key   string
+	Value []byte
 }
 
 // PreparedSlot returns the WAL index for the PREPARED phase at a given height.
@@ -43,16 +43,46 @@ func AbortSlot(height uint64) uint64 {
 }
 
 // Encode serializes a WalTx into bytes.
+// Format: [KeyLen(4 bytes)] [KeyBytes] [ValueLen(4 bytes)] [ValueBytes]
 func Encode(tx WalTx) ([]byte, error) {
-	return json.Marshal(tx)
+	keyLen := uint32(len(tx.Key))
+	valLen := uint32(len(tx.Value))
+
+	// 4 bytes for key len + key bytes + 4 bytes for val len + val bytes
+	buf := make([]byte, 4+keyLen+4+valLen)
+
+	binary.BigEndian.PutUint32(buf[0:4], keyLen)
+	copy(buf[4:4+keyLen], tx.Key)
+
+	binary.BigEndian.PutUint32(buf[4+keyLen:4+keyLen+4], valLen)
+	copy(buf[4+keyLen+4:], tx.Value)
+
+	return buf, nil
 }
 
 // Decode deserializes bytes into a WalTx.
 func Decode(data []byte) (WalTx, error) {
-	var tx WalTx
-	err := json.Unmarshal(data, &tx)
-	if err != nil {
-		return WalTx{}, fmt.Errorf("failed to decode WalTx: %w", err)
+	if len(data) < 4 {
+		return WalTx{}, fmt.Errorf("data too short for key length")
 	}
-	return tx, nil
+
+	keyLen := binary.BigEndian.Uint32(data[0:4])
+	if uint32(len(data)) < 4+keyLen+4 {
+		return WalTx{}, fmt.Errorf("data too short for key and value length")
+	}
+
+	key := string(data[4 : 4+keyLen])
+
+	valLen := binary.BigEndian.Uint32(data[4+keyLen : 4+keyLen+4])
+	if uint32(len(data)) < 4+keyLen+4+valLen {
+		return WalTx{}, fmt.Errorf("data too short for value body")
+	}
+
+	value := make([]byte, valLen)
+	copy(value, data[4+keyLen+4:4+keyLen+4+valLen])
+
+	return WalTx{
+		Key:   key,
+		Value: value,
+	}, nil
 }
